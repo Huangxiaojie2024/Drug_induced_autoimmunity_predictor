c0c4f11685e3042965647708a42f3f01c186c9d8
Huangxiaojie2024 <xj_huang@163.com>
2024-10-09 12:42:28 +0800

    Update auto_immu.py

--- auto_immu.py
+++ auto_immu.py
@@ -40,7 +40,7 @@ def get_descriptors(smiles):
     return np.array(descriptors)
 
 # LIME解释器初始化
-explainer = lime.lime_tabular.LimeTabularExplainer(
+explainer = lime_tabular.LimeTabularExplainer(
     training_data=Xtrain_std,
     feature_names=Xtrain_std.columns.tolist(),
     class_names=['DIA_negative', 'DIA_positive'],
